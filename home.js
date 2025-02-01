bc671545bc35439a9052a6f862ac88035a81286f
Gouresh33 <166603693+Gouresh33@users.noreply.github.com>
2025-02-01 11:44:16 +0530

    added more pages

--- home.js
+++ home.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("nav ul li a");
-    links.forEach(link => {
-        link.addEventListener("mouseover", function() {
-            this.style.color = "yellow";
-        });
-        link.addEventListener("mouseout", function() {
-            this.style.color = "white";
+    const buttons = document.querySelectorAll(".buttons a");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            alert("This functionality is not yet implemented.");
         });
     });
 });
